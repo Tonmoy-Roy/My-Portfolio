2b4eeb696cb10852d1a6cab4497f404ee6a5a024
Tonmoy-Roy <roytonmoy901@gmail.com>
2024-07-13 22:51:45 +0600

    commit changes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 function downloadCV() {
-    // Create a link element
-    const link = document.createElement("a");
-  
-    // Set the href attribute to the URL of the CV file
-    link.href = "./TR_CV.pdf";
-  
-    // Specify that the link should be downloaded when clicked
-    link.setAttribute("download", "TR_CV.pdf");
-  
-    // Append the link to the document body
-    document.body.appendChild(link);
-  
-    // Programmatically click the link to trigger the download
-    link.click();
-  
-    // Remove the link element from the document
-    document.body.removeChild(link);
-  }
-  
+  // Create a link element
+  const link = document.createElement("a");
+
+  // Set the href attribute to the URL of the CV file
+  link.href = "./TR_CV.pdf";
+
+  // Specify that the link should be downloaded when clicked
+  link.setAttribute("download", "TR_CV.pdf");
+
+  // Append the link to the document body
+  document.body.appendChild(link);
+
+  // Programmatically click the link to trigger the download
+  link.click();
+
+  // Remove the link element from the document
+  document.body.removeChild(link);
+}
+
